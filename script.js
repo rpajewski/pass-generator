a0d9764a6f7bfc53b2d6e63bd3f61c90f6a0b73e
Ricky Bobby <richard.pajewski@gmail.com>
2020-09-19 09:55:48 -0500

    initial js structure added

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
 // Assignment code here
+// get and store user input using prompts and confirms
+// generate random numbers
+// select items from arrays
+// implement control flow using fuctions, iterations and condition statements
+// When clicking generate button user is prompted for password criteria
+// Length of Password between 8 & 128
+// Confirm that at least one character type was selected
+// Generate Password Matching Criteria
+
+// Arrays of possible characters
+
+// Array of lowercase letters
+var lowerCaseChar = [
+  'a','b','c','d','e','f','g','h','i','j','k','l','m',
+  'n','o','p','q','r','s','t','u','v','w','x','y','z'
+];
+
+// Array of uppercase letters
+var upperCaseChar = [
+  'A','B','C','D','E','F','G','H','I','J','K','L','M',
+  'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+];
+
+// Array of numeric values
+var numericVal = ['0','1','2','3','4','5','6','7','8','9'];
+
+// Array of special characters
+var specialChar = [
+  '!','@','#','$','%','^','&','*','(',')','=','+',
+  '<','>','?','[',']','{','}','~',';',':','.',','
+];
+
 
 
 // Get references to the #generate element
